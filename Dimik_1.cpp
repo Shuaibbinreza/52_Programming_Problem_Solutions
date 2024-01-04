bb6db231feb4e1f80d32c153870bcddd7f5b387d
Shuaib <42146647+Shuaibbinreza@users.noreply.github.com>
2024-01-04 15:57:01 +0600

    Create Dimik_1.cpp

---
+++ Dimik_1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long int in;
+        cin >> in;
+        if(in % 2 == 0) cout << "even" << endl;
+        else    cout << "odd" << endl;
+    }
+    
+    return 0;
+}
