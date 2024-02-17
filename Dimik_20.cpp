ecaf308a26e0f7e2e6d7fd7bea35567cdbdeecb7
Shuaib <42146647+Shuaibbinreza@users.noreply.github.com>
2024-02-17 12:28:39 +0600

    Create Dimik_20.cpp

---
+++ Dimik_20.cpp
@@ -0,0 +1,25 @@
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+    char ch;
+
+    for (int i = 0; i < t; i++)
+    {
+        string str;
+
+        getline(cin, str);
+
+        int cnt = 0;
+
+        for (int j = 0; j < str.size(); j++)
+        {
+            if(str[j] == ' ')   cnt++;
+        }
+        
+        cout << "Count = " << cnt + 1 << endl;
+    }
+
+    return 0;
+}
